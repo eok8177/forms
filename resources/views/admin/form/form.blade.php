b953c7e37c864cb04d8b910dc526403d47ef6dd4
Sergey Markov <sergey.markov@gmail.com>
2019-10-24 21:56:03 +1100

    admin: added `description` field for a form

--- resources/views/admin/form/form.blade.php
+++ resources/views/admin/form/form.blade.php
@@ -4,6 +4,11 @@
   <input type="text" name="title" value="{{$form->title}}" class="form-control">
 </div>
 
+<div class="form-group">
+	<label for="title">Description</label>
+  {!! Form::textarea('description', $form->description, ['class' => 'form-control']) !!}
+</div>
+
 <div class="custom-control custom-checkbox custom-control-inline">
   {!! Form::hidden('is_active', 0) !!}
   {!! Form::checkbox('is_active', 1, $form->is_active, ['class' => 'custom-control-input', 'id' => 'is_active']) !!}
