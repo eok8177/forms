a6037a423f4eec63f59b69747de982e75eea3d8f
Evgen <eok8177@gmail.com>
2019-11-17 22:56:01 +0200

    front condition hide/show (start)

--- resources/js/components/formGUI/ControlComponent.vue
+++ resources/js/components/formGUI/ControlComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="controlItem form-group" :class="control.className">
+    <div class="controlItem form-group" :class="control.className" v-if="show">
         <component :is="controlInstance" v-model="control" :label-position="labelPosition"></component>
     </div>
 </template>
@@ -12,14 +12,54 @@
         props: ['control', 'labelPosition'],
         data: () => ({
             controlInstance: null,
+            show: true,
         }),
         created() {
+            let self = this;
             if (!CONTROL_TYPES[this.control.type]) {
                 console.error(`Control type ${this.control.type} doesn't exist to render.`);
                 return;
             }
             // set control
             this.controlInstance = CONTROL_TYPES[this.control.type].source;
+
+            if (this.control.isCalculated) {
+                console.log(this.control);
+                this.show = this.control.condition.action_type == 'show' ? false : true;
+
+                _.each(this.control.condition.rules, (rule) => {
+
+                    // add Event to target field
+                    $('body').on('change', 'input[name="'+rule.fieldId+'"]', function(){
+                        let valid = false;
+                        switch(rule.operator) {
+                          case 'is':
+                            if (parseInt($(this).val()) == rule.value) valid = true;
+                            break;
+                          case 'is_not':
+                            if (parseInt($(this).val()) != rule.value) valid = true;
+                            break;
+                          case 'greater':
+                            if (parseInt($(this).val()) > rule.value) valid = true;
+                            break;
+                          case 'less':
+                            if (parseInt($(this).val()) < rule.value) valid = true;
+                            break;
+                          default:
+                            valid = false;
+                        }
+                        self.toggleField(valid);
+                    });
+
+                });
+
+            }
+        },
+        methods: {
+            toggleField(valid) {
+                //TODO: add login on this.control.condition.logic_type AND many rules
+                this.show = this.control.condition.action_type == 'show' ? valid : !valid;
+            }
         }
     }
 </script>
