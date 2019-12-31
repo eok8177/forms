e8c33871dd20a330ad70afa8b5bba4fa2b633654
Sergey Markov <sergey.markov@gmail.com>
2019-12-31 13:16:47 +1100

    Dashboard: show by which form current filter is applied

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,7 +11,8 @@
         <th scope="col">Responses</th>
         <th scope="col">
           <div class="dropdown">
-            <a class="btn btn-outline-info dropdown-toggle" href="#" role="button" id="dropdownForms" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Filter by Form</a>
+            <a class="btn btn-outline-info dropdown-toggle" href="#" role="button" id="dropdownForms" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ $selected_form_name }}</a>
+			
             <div class="dropdown-menu" aria-labelledby="dropdownForms">
               <a class="dropdown-item" href="{{ route('admin.dashboard') }}">All Forms</a>
               @foreach($select_forms as $id => $item)
