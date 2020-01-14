41ea142b9ec8d5fe0471ae72d9e7d02e516fa203
Evgen <eok8177@gmail.com>
2020-01-14 17:40:00 +0200

    return login via username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            // $table->string('login')->unique();
+            $table->string('login')->unique();
             $table->string('email')->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
