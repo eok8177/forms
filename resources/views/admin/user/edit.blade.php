84da1ccd4a5b33b2039da2d8f06925d8b29bb16b
Evgen <eok8177@gmail.com>
2020-01-21 17:56:04 +0200

    user edit show last_logged_in

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -4,6 +4,11 @@
 <h2 class="page-header">@lang('message.user') <small>{{ $user->login }}</small></h2>
 
 {!! Form::open(['route' => ['admin.user.update', $user->id], 'method' => 'PUT', 'class' => 'form-horizontal']) !!}
+
+    <div class="form-group">
+      <label for="name">@lang('message.loged_at'): {{$user->last_logged_in}}</label>
+    </div>
+
   @include('admin.user.form')
 {!! Form::close() !!}
 
