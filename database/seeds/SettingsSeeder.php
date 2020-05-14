e4d6c805262a067116958db032882e3cdc842130
Sergey Markov <sergeyatrwav@gmail.com>
2020-05-14 23:54:36 +1000

    Seeder Settings updated

--- database/seeds/SettingsSeeder.php
+++ database/seeds/SettingsSeeder.php
@@ -13,6 +13,9 @@ class SettingsSeeder extends Seeder
     {
         DB::table('settings')->insert([
             ['id' => 1, 'key' => 'admin_email', 'name' => 'Admin email', 'value' => 'sergey.markov@gmail.com'],
+            ['id' => 2, 'key' => 'date_format', 'name' => 'Date Format', 'value' => 'dd/mm/yyyy'],
+            ['id' => 3, 'key' => 'key_map', 'name' => 'Google MAP key', 'value' => 'AIzaSyDnxGiPdH3lTiOVu98kJxvn3h8Oezlw3w4'],
+            ['id' => 4, 'key' => 'form_type_colors', 'name' => 'Form Type Colors', 'value' => '#fff, #f00, #0f0, #00f, #000']
         ]);
     }
 }
