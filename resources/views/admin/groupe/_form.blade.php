7d7d0e8de85f583a96f5af6dc1ac680b04bc0448
Sergey Markov <sergey.markov@gmail.com>
2020-05-20 22:26:16 +1000

    Form group: minor changes

--- resources/views/admin/groupe/_form.blade.php
+++ resources/views/admin/groupe/_form.blade.php
@@ -1,9 +1,10 @@
 <div class="form-group">
-  <label for="title">Name</label>
+  <label for="title">Group name</label>
   <input type="text" class="form-control" name="name" value="{{$group->name}}">
 </div>
-
+<br />
 <div class="form-group">
+<label>Managers</label>
 @foreach($managers as $manager)
   <div class="custom-control custom-checkbox">
     <input type="checkbox" class="custom-control-input" 
