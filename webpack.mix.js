946ff110b7a2707160477100586f130ba58ce233
Evgen <eok8177@gmail.com>
2020-07-15 22:08:16 +0300

    compiled style.scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.options({
+    processCssUrls: false
+});
+
 mix.webpackConfig({
     output: {
         // Chunks in webpack
@@ -21,7 +25,8 @@ mix.webpackConfig({
 
 mix
    // .js('resources/js/admin.js', 'public/js')
-   .js('resources/js/app.js', 'public/js')
+   // .js('resources/js/app.js', 'public/js')
    // .sass('resources/sass/app.scss', 'public/css')
    // .sass('resources/sass/admin.scss', 'public/css')
+   .sass('public/css/style.scss', 'public/css')
    ;
