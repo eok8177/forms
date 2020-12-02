d8d531323b64dab624ea014ee83efb83e3978df1
Sergey Markov <sergey.markov@gmail.com>
2020-12-02 15:32:32 +1100

    GUI fix: calc error (section)

--- resources/js/components/formGUI/SectionComponent.vue
+++ resources/js/components/formGUI/SectionComponent.vue
@@ -88,7 +88,7 @@
                 _.forEach(this.$parent.form.sections, function(section, key) {
                     if (section.isDynamic) { // search in Dynamic section
                         _.forEach(section.instances, function(instance) {
-                            _.forEach(instance, function(value) {
+                             _.forEach(instance, function(row) {
                                 _.forEach(row.controls, function(control) {
                                     if (control.fieldName == fieldName) {
                                         field = control;
